1cac4f250e0614c8332fcd80c5cc866e97beae80
skanet <sky.net@gmx.com>
2018-07-09 01:04:20 +0200

    Edit k8s script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,4 +2,4 @@ docker build -t  jenkinstest .
 docker tag jenkinstest metabol/images:jenkinstest
 docker push metabol/images:jenkinstest
 docker run --name jenkinstest-con metabol/images:jenkinstest
-#docker container rm jenkinstest-con -f
+docker container rm jenkinstest-con -f
