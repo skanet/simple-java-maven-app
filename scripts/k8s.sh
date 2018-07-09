9664dea5d6099e2d69d575ec0be95c50117f6ed4
skanet <sky.net@gmx.com>
2018-07-09 16:50:37 +0200

    added SVC

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -1,2 +1,4 @@
 kubectl run --image=metabol/images:jenkinstest --replicas=3 jenkinstest-pod
+kubectl expose deployment/jenkinstest-pod --port=80 --name=jenkinstest-svc --type=LoadBalancer
 kubectl get pods
+kubectl get svc
